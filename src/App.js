ca165554bcea2c42ad6fa347bf96b6409e8d986f
srivarshabachu <srivarshabachu@gmail.com>
2024-03-28 12:54:37 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import axios from 'axios'
 
 function App() {
+  let a = () => {
+    let data = {
+      Email: "ramcharan.pathiri@gmail.com",
+      Username: "ram",
+      Password: "93915616@Ram"
+    }
+    axios.post("https://localhost:7235/api/Authentication/Register?role=User", data)
+      .then((response) => {
+        console.log(response)
+      })
+      .catch((err) => {
+        console.log("error is ", err);
+      })
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button className="sucess" onClick={a}> submit</button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
